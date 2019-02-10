a8ae3a1c56d75677c4b48e7c8da958b80dba379a
aditihaiman <ahaiman00@stuy.edu>
2019-02-09 22:16:27 -0500

    changing to 1D fixing queenInDanger

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,24 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class QueenBoard {
 
   public static void main(String[] args) {
     QueenBoard board1 = new QueenBoard(10);
-    board1.addQueen(5, 5);
+    //board1.addQueen(5, 5);
     System.out.println(board1);
     board1.addQueen(2, 5);
-    board1.addQueen(0, 0);
+    board1.addQueen(3, 6);
+    //board1.addQueen(0, 0);
     System.out.println(board1);
-    System.out.println(board1.queenInDanger(0, 0));
+    //System.out.println(board1.queenInDanger(2, 5));
+    board1 = new QueenBoard(4);
+    System.out.println(board1.solve(0,0));
 
   }
 
-  private int[][] board;
+  private int[] board;
 
   public QueenBoard(int size) {
-    board = new int[size][size];
+    board = new int[size];
     for (int x = 0; x < size; x++) {
-      for (int y = 0; y < size; y++) {
-        board[x][y] = 0;
-      }
+        board[x] = -1;
     }
   }
 
@@ -52,9 +56,21 @@ public class QueenBoard {
   *        true when the board is solveable, and leaves the board in a solved state
   *@throws IllegalStateException when the board starts with any non-zero value
   */
-  public boolean solve(){
-    if (!isEmpty()) throw new IllegalStateException();
-    return false;
+  public boolean solve(int r, int c){
+    //if (countQueens()!=0) throw new IllegalStateException();
+    if (countQueens()==board.length) return true;
+    else if (r>=board.length && c >= board.length) return false;
+    for (int x = r; x < board.length; x++) {
+      for (int y = c; y < board.length; y++) {
+        if (board[x][y]==0) addQueen(x,y);
+        System.out.println(this.toString());
+        if (queenInDanger(x, y)){
+          removeQueen(x,y);
+          //return solve(r, c+1);
+        }
+      }
+    }
+    return solve(r+1, c+1);
   }
 
   /**
@@ -63,20 +79,23 @@ public class QueenBoard {
   */
   public int countSolutions(){
     int output = 0;
-    if (!isEmpty()) throw new IllegalStateException();
+    //if (countQueens()!=0) throw new IllegalStateException();
     return output;
   }
 
 
 
-  private boolean queenInDanger(int r, int c) {
-    for (int x = 0; x < board.length; x++) {
-      for (int y = 0; y < board[0].length; y++) {
-        if (x!=r&&board[x][c]==1) return true;
-        if (y!=c&&board[r][y]==1) return true;
-        if (x!=r && y!=c && (c-y)==(r-x) && board[x][y]==1) return true;
-      }
-    }
+  private boolean queensInDanger(int idx) {
+     for (int x = 0; x < board.length; x++) {
+       if (board[x]==board[idx]) return true;
+       if (x == (board[idx] - board[x])) return true;
+     }
+    //   for (int y = 0; y < board[0].length; y++) {
+    //     if (x!=r&&board[x][c]==1) return true;
+    //     if (y!=c&&board[r][y]==1) return true;
+    //     if (x!=r && y!=c && (c-y)==(r-x) && board[x][y]==1) return true;
+    //     if (x!=r && y!=c && (c-y)==(x-r) && board[x][y]==1) return true;
+    //   }
     return false;
   }
 
@@ -90,13 +109,14 @@ public class QueenBoard {
     return true;
   }
 
-  private boolean isEmpty() {
-    for (int x = 0; x < board.length; x++) {
-      for (int y = 0; y < board[x].length; y++) {
-        if (board[x][y] != 0) return false;
-      }
-    }
-    return true;
-  }
+  // private int countQueens() {
+  //   int output = 0;
+  //   for (int x = 0; x < board.length; x++) {
+  //     for (int y = 0; y < board[x].length; y++) {
+  //       if (board[x][y] == 1) output++;
+  //     }
+  //   }
+  //   return output;
+  // }
 
 }
