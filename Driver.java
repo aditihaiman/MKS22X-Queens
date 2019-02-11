4461eaeb756017ce1c5d0144a6e3bf987045b6f0
aditihaiman <ahaiman00@stuy.edu>
2019-02-11 10:04:11 -0500

    testing runtime

--- Driver.java
+++ Driver.java
@@ -3,16 +3,16 @@ public class Driver {
   public static void main(String[] args) {
     QueenBoard board1 = new QueenBoard(10);
     // board1.addQueen(5, 4);
-    //  System.out.println(board1);
-    //  board1.addQueen(6, 9);
+    // System.out.println(board1);
+    // board1.addQueen(6, 9);
     // System.out.println(board1);
     // System.out.println(board1.queenInDanger(9));
-    //System.out.println(board1.toString1());
-    board1 = new QueenBoard(10);
+    // System.out.println(board1.toString1());
+    board1 = new QueenBoard(14);
     // board1.addQueen(0, 0);
-    //board1.addQueen(1, 0);
+    // board1.addQueen(1, 0);
     System.out.println(board1.solve());
-    //System.out.println(board1);
+    // System.out.println(board1);
     System.out.println(board1.countSolutions());
 
   }
